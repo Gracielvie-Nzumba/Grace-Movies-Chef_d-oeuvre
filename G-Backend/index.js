6d8fa8531d711761f7949aebb59e6b981e35add5
Gracevie Nzumba <Gracevienzumba@gmail.com>
2024-03-07 17:25:55 +0200

    ajout de dependaances

--- G-Backend/index.js
+++ G-Backend/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const multer = require ('multer');
+const request  = require('supertest');
+const {Prisma} = require ('Prisma');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const axios = require('axios')
 const path = require('path');
-
 const app = express();
+const app = require('../app');
 
 app.use(express.static(path.join(__dirname, '../Frontend/src')));
 
